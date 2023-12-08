2e4eb683cb6972da5c6cdc31b265b307945cfe33
Artem Khimin <a.khimin@yandex.ru>
2023-12-08 03:30:56 +0300

    add axes 3d

---
+++ axes01.py
@@ -0,0 +1,9 @@
+import numpy as np
+import  matplotlib.pyplot as plt
+from  mpl_toolkits.mplot3d import Axes3D
+
+fig=plt.figure(figsize=(7,4))
+# ax_3d=Axes3D(fig)
+ax_3d=fig.add_subplot(projection='3d')
+
+plt.show()
